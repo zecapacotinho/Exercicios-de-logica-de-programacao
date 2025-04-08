594a1ab075fc72ce4748ab62e37fbb51d9850545
João Carlos Apolinario da Silva <jcasilva1349@gmail.com>
2025-04-08 20:43:52 -0300

    atz

---
+++ Função que retorna pares de um range/script.js
@@ -0,0 +1,12 @@
+function ListaRange(){
+    let start = 2
+    let end = 12
+    for(let i = start; i < end; i++){
+        if(i % 2 === 0){
+            console.log(i)
+        }
+    }
+}
+ListaRange()
+
+
