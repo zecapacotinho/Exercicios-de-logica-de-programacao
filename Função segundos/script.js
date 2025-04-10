5df5a0ceda2caa680fcfcca666e25b9bf7983775
João Carlos Apolinario da Silva <jcasilva1349@gmail.com>
2025-04-10 17:49:41 -0300

    atz

---
+++ Função segundos/script.js
@@ -0,0 +1,8 @@
+function Hora_Segundo(){
+    let h = 1
+    let m = 30
+    let s = 25
+    let resultado = (h * 3600) + (m * 60) + s
+    console.log(`O total de segundos de "${h}" "${m}" "${s}" é de ${resultado}`)
+}
+Hora_Segundo()
