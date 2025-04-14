662ca13f8c883af725ea4eb55b4e0057584c8fd2
João Carlos Apolinario da Silva <jcasilva1349@gmail.com>
2025-04-14 19:06:04 -0300

    atz

---
+++ CÓDIGO CÉSAR/script.js
@@ -0,0 +1,25 @@
+function CodigoCesar(){
+    let num = window.document.querySelector('#num').value
+    let res = window.document.querySelector('#result')
+
+    if(!num.trim()){
+        window.alert('Por favor, coloque um valor!')
+    }
+    else{
+        res.innerHTML = ''
+        let def = num
+        for(let i = 0; i < def.length; i++){
+            texto = def[i]
+            let valor = texto.charCodeAt(0)
+
+            if(valor >= 97 && valor <= 122){
+                let codigo = ((valor - 97 + 3) % 26) + 97
+                res.innerHTML += String.fromCharCode(codigo)
+            }
+            else{
+                res.innerHTML = def
+            }
+
+        }
+    }
+}
