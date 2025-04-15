24da89115384d15b6605c94c37650bb6f69a384b
João Carlos Apolinario da Silva <jcasilva1349@gmail.com>
2025-04-15 19:10:31 -0300

    atz

---
+++ Contador individual de vogais/script.js
@@ -0,0 +1,23 @@
+function ContadorIndividual(texto){
+    let contador = {
+        a: 0,
+        e: 0,
+        i: 0,
+        o: 0,
+        u: 0,
+    }
+
+    for(let letra of texto.toLowerCase()){
+        if(letra in contador){
+            contador[letra]++
+        }
+    }
+    
+    let resultado = ''
+    for(let vogal in contador){
+        resultado += `${vogal}: ${contador[vogal]}\n`
+    }
+    
+    return resultado
+}
+console.log(ContadorIndividual('seios'))
