cf117e84bc6599c2e906bdac4c7aaac0a22142d6
João Carlos Apolinario da Silva <jcasilva1349@gmail.com>
2025-04-15 19:45:42 -0300

    atz

---
+++ Contador de consoantes/script.js
@@ -0,0 +1,15 @@
+function ContadorCosoante(texto){
+    let consoate = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q','r','s', 't', 'v', 'w', 'x', 'y', 'z']
+    let leitor = 0
+
+    for(let contador of texto.toLowerCase()){
+        if(consoate.includes(contador)){
+            leitor++
+        }
+    }
+
+    return (leitor === 0)
+        ? 'Não contém nenhuma consoante...'
+        : `No total tem ${leitor} consoantes.`
+}
+console.log(ContadorCosoante('rosa'))
