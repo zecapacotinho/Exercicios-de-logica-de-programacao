ba4bb5b025da9622e647d13c5b1edf70142d2b66
João Carlos Apolinario da Silva <jcasilva1349@gmail.com>
2025-04-15 20:10:23 -0300

    atz

--- Contador de palavras/script.js
+++ Contador de palavras/script.js
@@ -1,5 +1,5 @@
 function ContadorPalavra(texto){
     let palavras = texto.trim().split(' ').filter(p => p !== '')
-    return `A frase "${texto}", tem no total ${palavras.length} palavras`
+    return `A frase "${texto}", tem no total ${palavras.length} palavras.`
 }
 console.log(ContadorPalavra('eu gosto de sorvete'))
