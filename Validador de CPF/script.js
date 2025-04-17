05b8515a36d3b4958b191968a10fa6d4df6b0fd7
João Carlos Apolinario da Silva <jcasilva1349@gmail.com>
2025-04-16 21:47:01 -0300

    atz

--- Validador de CPF/script.js
+++ Validador de CPF/script.js
@@ -20,11 +20,29 @@ function VerificadorCPF(){
     }
     else{
         let valor = num.value.replace(/\D/g, '')
-        let soma = 0
-        for(let i = 0; i <= 9; i++){
-            soma += Number(valor[i]) * (10 - i)
+        let soma1 = 0
+        let soma2 = 0
+
+        for(let i = 0; i < 9; i++){
+            soma1 += Number(valor[i]) * (10 - i)
         }
-        res.innerHTML = soma
+        let valor1 = (soma1 * 10) % 11
+        let digito1 = (valor1 === 10 || valor1 === 11)
+            ? 0
+            : valor1
         
+        let valor2 = valor + digito1
+        for(let j = 0; j < 10; j++){
+            soma2 += Number(valor2[j]) * (11 - j)
+        }
+        let valor3 = (soma2 * 10) % 11
+        let digito2 = (valor3 === 10 || valor3 === 11)
+            ? 0
+            : valor3
+        
+        let CPFinal = valor.substring(9, 11)
+        res.innerHTML = (digito1 === Number(CPFinal[0]) && digito2 === Number(CPFinal[1]))
+            ? '<span style="color: green;">CPF Válido</span>'
+            : '<span style="color: red;">CPF Inválido</span>'
     }
 }
